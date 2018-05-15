f7198a507bdd77f3167ba93fd3c6093a57d80069
androiddrew <bednar.andrew@gmail.com>
2018-05-14 21:48:09 -0400

    Bump version: 0.2.1 → 0.3.0

--- apistar_mail/__init__.py
+++ apistar_mail/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Drew Bednar"""
 __email__ = 'drew@androiddrew.com'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 from .mail import MailComponent, Message, Mail  # NOQA: F401
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = [
 
 setup(
     name='apistar-mail',
-    version='0.2.1',
+    version='0.3.0',
     description="A simple email Component for APIStar",
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
